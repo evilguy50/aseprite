evilguy50/aseprite

commit a2cbf3f2858bb407041383fdf711cf3d634f3562

Fix crash using --trim without --split-layers

src/app/document_exporter.cpp
@@ -395,10 +395,13 @@ void DocumentExporter::captureSamples(Samples& samples)
         doc::color_t refColor = 0;
 
         if (m_trimCels) {
-          if (layer->isBackground())
+          if (sprite->backgroundLayer() &&
+              sprite->backgroundLayer()->isVisible()) {
             refColor = get_pixel(sampleRender, 0, 0);
-          else
+          }
+          else {
             refColor = sprite->transparentColor();
+          }
         }
         else if (m_ignoreEmptyCels)
           refColor = sprite->transparentColor();

commit 62dc674c27483695b369c36dfefe989d7910f38a

Fix bug introduced in a2cbf3f2858bb407041383fdf711cf3d634f3562: trim transparent layers correctly

src/app/document_exporter.cpp
@@ -402,8 +402,11 @@ void DocumentExporter::captureSamples(Samples& samples)
         doc::color_t refColor = 0;
 
         if (m_trimCels) {
-          if (sprite->backgroundLayer() &&
-              sprite->backgroundLayer()->isVisible()) {
+          if ((layer &&
+               layer->isBackground()) ||
+              (!layer &&
+               sprite->backgroundLayer() &&
+               sprite->backgroundLayer()->isVisible())) {
             refColor = get_pixel(sampleRender, 0, 0);
           }
           else {