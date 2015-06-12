evilguy50/aseprite

commit d732f5b05f33ed1fce4c8b900a54f617bfb4c5ae

Use get_pixel_address_fast() in ImageIterator

src/doc/image_impl.h
@@ -14,7 +14,6 @@
 #include "doc/blend.h"
 #include "doc/image.h"
 #include "doc/image_bits.h"
-#include "doc/image_iterator.h"
 #include "doc/palette.h"
 
 namespace doc {
@@ -252,26 +251,10 @@ namespace doc {
     }
   }
 
+  void copy_bitmaps(Image* dst, const Image* src, gfx::Clip area);
   template<>
   inline void ImageImpl<BitmapTraits>::copy(const Image* src, gfx::Clip area) {
-    if (!area.clip(width(), height(), src->width(), src->height()))
-      return;
-
-    // Copy process
-    ImageConstIterator<BitmapTraits> src_it(src, area.srcBounds(), area.src.x, area.src.y);
-    ImageIterator<BitmapTraits> dst_it(this, area.dstBounds(), area.dst.x, area.dst.y);
-
-    int end_x = area.dst.x+area.size.w;
-
-    for (int end_y=area.dst.y+area.size.h;
-         area.dst.y<end_y;
-         ++area.dst.y, ++area.src.y) {
-      for (int x=area.dst.x; x<end_x; ++x) {
-        *dst_it = *src_it;
-        ++src_it;
-        ++dst_it;
-      }
-    }
+    copy_bitmaps(this, src, area);
   }
 
 } // namespace doc

src/doc/image_iterator.h
@@ -8,11 +8,13 @@
 #define DOC_IMAGE_ITERATOR_H_INCLUDED
 #pragma once
 
-#include "gfx/point.h"
-#include "gfx/rect.h"
 #include "doc/color.h"
 #include "doc/image.h"
+#include "doc/image_impl.h"
 #include "doc/image_traits.h"
+#include "doc/primitives_fast.h"
+#include "gfx/point.h"
+#include "gfx/rect.h"
 
 #include <cstdlib>
 #include <iterator>
@@ -50,7 +52,7 @@ namespace doc {
 
     ImageIteratorT(const Image* image, const gfx::Rect& bounds, int x, int y) :
       m_image(const_cast<Image*>(image)),
-      m_ptr((pointer)image->getPixelAddress(x, y)),
+      m_ptr(get_pixel_address_fast<ImageTraits>(image, x, y)),
       m_x(x),
       m_y(y),
       m_xbegin(bounds.x),
@@ -104,7 +106,7 @@ namespace doc {
         ++m_y;
 
         if (m_y < m_image->height())
-          m_ptr = (pointer)m_image->getPixelAddress(m_x, m_y);
+          m_ptr = get_pixel_address_fast<ImageTraits>(m_image, m_x, m_y);
       }
 
       return *this;
@@ -287,7 +289,7 @@ namespace doc {
 
     ImageIteratorT(const Image* image, const gfx::Rect& bounds, int x, int y) :
       m_image(const_cast<Image*>(image)),
-      m_ptr((pointer)image->getPixelAddress(x, y)),
+      m_ptr(get_pixel_address_fast<BitmapTraits>(image, x, y)),
       m_x(x),
       m_y(y),
       m_subPixel(x % 8),
@@ -341,7 +343,7 @@ namespace doc {
         ++m_y;
 
         if (m_y < m_image->height())
-          m_ptr = (pointer)m_image->getPixelAddress(m_x, m_y);
+          m_ptr = get_pixel_address_fast<BitmapTraits>(m_image, m_x, m_y);
         else
           ++m_ptr;
       }

src/doc/primitives_fast.h
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (c) 2001-2014 David Capello
+// Copyright (c) 2001-2015 David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -13,6 +13,13 @@
 
 namespace doc {
   class Image;
+
+  template<class Traits>
+  inline typename Traits::address_t get_pixel_address_fast(const Image* image, int x, int y) {
+    ASSERT(x >= 0 && x < image->width());
+    ASSERT(y >= 0 && y < image->height());
+    return (((ImageImpl<Traits>*)image)->address(x, y));
+  }
 
   template<class Traits>
   inline typename Traits::pixel_t get_pixel_fast(const Image* image, int x, int y) {

src/doc/CMakeLists.txt
@@ -31,6 +31,7 @@ add_library(doc-lib
   frame_tags.cpp
   handle_anidir.cpp
   image.cpp
+  image_impl.cpp
   image_io.cpp
   images_collector.cpp
   layer.cpp

src/doc/image_impl.cpp
@@ -1,0 +1,40 @@
+// Aseprite Document Library
+// Copyright (c) 2001-2015 David Capello
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "doc/image_impl.h"
+
+#include "doc/image_iterator.h"
+#include "doc/image_traits.h"
+
+namespace doc {
+
+void copy_bitmaps(Image* dst, const Image* src, gfx::Clip area)
+{
+  if (!area.clip(dst->width(), dst->height(), src->width(), src->height()))
+    return;
+
+  // Copy process
+  ImageConstIterator<BitmapTraits> src_it(src, area.srcBounds(), area.src.x, area.src.y);
+  ImageIterator<BitmapTraits> dst_it(dst, area.dstBounds(), area.dst.x, area.dst.y);
+
+  int end_x = area.dst.x+area.size.w;
+
+  for (int end_y=area.dst.y+area.size.h;
+       area.dst.y<end_y;
+       ++area.dst.y, ++area.src.y) {
+    for (int x=area.dst.x; x<end_x; ++x) {
+      *dst_it = *src_it;
+      ++src_it;
+      ++dst_it;
+    }
+  }
+}
+
+} // namespace doc

src/doc/image_traits.h
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (c) 2001-2014 David Capello
+// Copyright (c) 2001-2015 David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -29,7 +29,7 @@ namespace doc {
 
     static const pixel_t min_value = 0x00000000l;
     static const pixel_t max_value = 0xffffffffl;
-    
+
     static inline int getRowStrideBytes(int pixels_per_row)
     {
       return bytes_per_pixel * pixels_per_row;

commit 829e4439eb203cfb59ac526761c154f30179f198

Fix circular dependencies between image_impl.h <-> image_iterator.h
This problem was introduced in d732f5b05f33ed1fce4c8b900a54f617bfb4c5ae

Now image_iterator.h and image_bits.h use forward declarations. And if
someone want to use the Image implementation, ImageBits, LockImageBits,
ImageIterators, or some function inside primitive_fast.h, they just have
to include image_impl.h file.

src/app/cmd/clear_mask.cpp
@@ -14,8 +14,7 @@
 #include "app/cmd/clear_cel.h"
 #include "app/document.h"
 #include "doc/cel.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/layer.h"
 #include "doc/mask.h"
 #include "doc/primitives.h"

src/app/cmd/remap_colors.cpp
@@ -14,7 +14,6 @@
 #include "doc/cel.h"
 #include "doc/cels_range.h"
 #include "doc/image.h"
-#include "doc/image_bits.h"
 #include "doc/remap.h"
 #include "doc/sprite.h"
 

src/app/commands/filters/filter_manager_impl.h
@@ -11,8 +11,7 @@
 
 #include "base/exception.h"
 #include "base/unique_ptr.h"
-#include "doc/image_bits.h"
-#include "doc/image_traits.h"
+#include "doc/image_impl.h"
 #include "doc/pixel_format.h"
 #include "doc/site.h"
 #include "filters/filter_indexed_data.h"

src/app/tools/ink_processing.h
@@ -8,8 +8,8 @@
 #include "app/modules/palettes.h"
 #include "app/tools/shade_table.h"
 #include "app/tools/shading_options.h"
+#include "doc/image_impl.h"
 #include "doc/palette.h"
-#include "doc/primitives_fast.h"
 #include "doc/rgbmap.h"
 #include "doc/sprite.h"
 #include "filters/neighboring_pixels.h"

src/app/tools/tool_box.cpp
@@ -25,7 +25,7 @@
 #include "doc/algorithm/polygon.h"
 #include "doc/brush.h"
 #include "doc/compressed_image.h"
-#include "doc/image.h"
+#include "doc/image_impl.h"
 #include "doc/mask.h"
 #include "fixmath/fixmath.h"
 

src/app/ui/color_selector.cpp
@@ -30,8 +30,7 @@
 #include "app/ui_context.h"
 #include "base/bind.h"
 #include "base/scoped_value.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/palette.h"
 #include "doc/sprite.h"
 #include "gfx/border.h"

src/app/ui/editor/cursor.cpp
@@ -31,8 +31,7 @@
 #include "doc/algo.h"
 #include "doc/brush.h"
 #include "doc/cel.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/layer.h"
 #include "doc/mask_boundaries.h"
 #include "doc/primitives.h"

src/app/util/new_image_from_mask.cpp
@@ -12,9 +12,7 @@
 #include "app/util/new_image_from_mask.h"
 
 #include "app/document.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
-#include "doc/image_traits.h"
+#include "doc/image_impl.h"
 #include "doc/mask.h"
 #include "doc/site.h"
 

src/doc/algorithm/resize_image.cpp
@@ -10,11 +10,10 @@
 
 #include "doc/algorithm/resize_image.h"
 
-#include "gfx/point.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/palette.h"
 #include "doc/rgbmap.h"
+#include "gfx/point.h"
 
 namespace doc {
 namespace algorithm {

src/doc/algorithm/rotate.cpp
@@ -13,8 +13,7 @@
 
 #include "fixmath/fixmath.h"
 #include "doc/blend.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/primitives.h"
 #include "doc/primitives_fast.h"
 

src/doc/algorithm/rotsprite.cpp
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (c) 2001-2014 David Capello
+// Copyright (c) 2001-2015 David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -11,10 +11,8 @@
 #include "base/unique_ptr.h"
 #include "doc/algorithm/rotate.h"
 #include "doc/blend.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/primitives.h"
-#include "doc/primitives_fast.h"
 
 namespace doc {
 namespace algorithm {
@@ -152,7 +150,7 @@ static void image_scale2x(Image* dst, const Image* src, int src_w, int src_h)
     case IMAGE_BITMAP:    image_scale2x_tpl<BitmapTraits>(dst, src, src_w, src_h); break;
   }
 }
-  
+
 void rotsprite_image(Image* bmp, Image* spr,
   int x1, int y1, int x2, int y2,
   int x3, int y3, int x4, int y4)

src/doc/conversion_she.cpp
@@ -14,7 +14,6 @@
 #include "doc/algo.h"
 #include "doc/blend.h"
 #include "doc/color_scales.h"
-#include "doc/image.h"
 #include "doc/image_impl.h"
 #include "doc/palette.h"
 #include "doc/rgbmap.h"

src/doc/doc.h
@@ -18,7 +18,7 @@
 #include "doc/frame_tag.h"
 #include "doc/frame_tags.h"
 #include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/image_ref.h"
 #include "doc/layer.h"
 #include "doc/mask.h"

src/doc/image_bits.h
@@ -8,10 +8,11 @@
 #define DOC_IMAGE_BITS_H_INCLUDED
 #pragma once
 
-#include "doc/image.h"
-#include "doc/image_iterator.h"
-
 namespace doc {
+
+  class Image;
+  template<typename ImageTraits> class ImageIterator;
+  template<typename ImageTraits> class ImageConstIterator;
 
   template<typename ImageTraits>
   class ImageBits {
@@ -144,7 +145,7 @@ namespace doc {
     const gfx::Rect& bounds() const { return m_bits.bounds(); }
 
     Image* image() { return m_bits.image(); }
-    
+
   private:
     Bits m_bits;
 

src/doc/image_impl.h
@@ -14,9 +14,12 @@
 #include "doc/blend.h"
 #include "doc/image.h"
 #include "doc/image_bits.h"
+#include "doc/image_iterator.h"
 #include "doc/palette.h"
 
 namespace doc {
+
+  template<typename ImageTraits> class LockImageBits;
 
   template<class Traits>
   class ImageImpl : public Image {

src/doc/image_iterator.h
@@ -9,9 +9,6 @@
 #pragma once
 
 #include "doc/color.h"
-#include "doc/image.h"
-#include "doc/image_impl.h"
-#include "doc/image_traits.h"
 #include "doc/primitives_fast.h"
 #include "gfx/point.h"
 #include "gfx/rect.h"
@@ -22,6 +19,8 @@
 #include <iostream>
 
 namespace doc {
+
+  class Image;
 
   template<typename ImageTraits,
            typename PointerType,

src/doc/mask.cpp
@@ -11,8 +11,7 @@
 #include "doc/mask.h"
 
 #include "base/memory.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 
 #include <cstdlib>
 #include <cstring>

src/doc/mask_boundaries.cpp
@@ -10,8 +10,7 @@
 
 #include "doc/mask_boundaries.h"
 
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 
 namespace doc {
 

src/doc/primitives.cpp
@@ -13,7 +13,6 @@
 #include "doc/algo.h"
 #include "doc/blend.h"
 #include "doc/brush.h"
-#include "doc/image.h"
 #include "doc/image_impl.h"
 #include "doc/palette.h"
 #include "doc/rgbmap.h"

src/doc/primitives_fast.h
@@ -9,15 +9,16 @@
 #pragma once
 
 #include "doc/color.h"
-#include "doc/image_impl.h"
 
 namespace doc {
   class Image;
+  template<typename ImageTraits> class ImageImpl;
 
   template<class Traits>
   inline typename Traits::address_t get_pixel_address_fast(const Image* image, int x, int y) {
     ASSERT(x >= 0 && x < image->width());
     ASSERT(y >= 0 && y < image->height());
+
     return (((ImageImpl<Traits>*)image)->address(x, y));
   }
 

src/doc/sprite.cpp
@@ -16,7 +16,7 @@
 #include "doc/cel.h"
 #include "doc/cels_range.h"
 #include "doc/frame_tag.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/layer.h"
 #include "doc/palette.h"
 #include "doc/primitives.h"

src/filters/convolution_matrix_filter.cpp
@@ -15,9 +15,8 @@
 #include "filters/filter_indexed_data.h"
 #include "filters/filter_manager.h"
 #include "filters/neighboring_pixels.h"
-#include "doc/image.h"
+#include "doc/image_impl.h"
 #include "doc/palette.h"
-#include "doc/primitives_fast.h"
 #include "doc/rgbmap.h"
 
 namespace filters {

src/filters/median_filter.cpp
@@ -12,14 +12,13 @@
 #include "filters/median_filter.h"
 
 #include "base/memory.h"
+#include "doc/image_impl.h"
+#include "doc/palette.h"
+#include "doc/rgbmap.h"
 #include "filters/filter_indexed_data.h"
 #include "filters/filter_manager.h"
 #include "filters/neighboring_pixels.h"
 #include "filters/tiled_mode.h"
-#include "doc/image.h"
-#include "doc/palette.h"
-#include "doc/primitives_fast.h"
-#include "doc/rgbmap.h"
 
 #include <algorithm>
 

src/render/quantization.cpp
@@ -11,8 +11,7 @@
 #include "render/quantization.h"
 
 #include "doc/blend.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/images_collector.h"
 #include "doc/layer.h"
 #include "doc/palette.h"
@@ -25,8 +24,8 @@
 
 #include <algorithm>
 #include <limits>
-#include <vector>
 #include <map>
+#include <vector>
 
 namespace render {
 

src/render/render.cpp
@@ -12,6 +12,7 @@
 
 #include "doc/doc.h"
 #include "doc/handle_anidir.h"
+#include "doc/image_impl.h"
 #include "gfx/clip.h"
 #include "gfx/region.h"
 

src/doc/image_tests.cpp
@@ -11,8 +11,7 @@
 #include <gtest/gtest.h>
 
 #include "base/unique_ptr.h"
-#include "doc/image.h"
-#include "doc/image_bits.h"
+#include "doc/image_impl.h"
 #include "doc/primitives.h"
 
 using namespace base;