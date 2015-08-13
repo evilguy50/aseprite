evilguy50/aseprite

commit 35229e99a6196d71b90ef2762abb32b0a164f594

Add opaque/transparent mode in context bar for transformations (fix #546)
With this change now we add a "mask" image/parameter in rotation
functions. In this way we can identify which specific pixels are
inside the original mask/selection, and in opaque mode we can
include/scale/rotate all those pixels inside the mask, whatever
value they are, even if they are the mask color.

Fixes #730

src/app/ui/editor/pixels_movement.cpp
@@ -47,9 +47,11 @@ static inline const base::Vector2d<double> point2Vector(const gfx::PointT<T>& pt
   return base::Vector2d<double>(pt.x, pt.y);
 }
 
-PixelsMovement::PixelsMovement(Context* context,
+PixelsMovement::PixelsMovement(
+  Context* context,
   Site site,
-  const Image* moveThis, const gfx::Point& initialPos,
+  const Image* moveThis,
+  const Mask* mask,
   const char* operationName)
   : m_reader(context)
   , m_site(site)
@@ -64,22 +66,25 @@ PixelsMovement::PixelsMovement(Context* context,
   , m_originalImage(Image::createCopy(moveThis))
   , m_maskColor(m_sprite->transparentColor())
 {
-  m_initialData = gfx::Transformation(gfx::Rect(initialPos, gfx::Size(moveThis->width(), moveThis->height())));
+  m_initialData = gfx::Transformation(mask->bounds());
   m_currentData = m_initialData;
+
+  m_initialMask = new Mask(*mask);
+  m_currentMask = new Mask(*mask);
+
+  m_rotAlgoConn =
+    Preferences::instance().selection.rotationAlgorithm.AfterChange.connect(
+      Bind<void>(&PixelsMovement::onRotationAlgorithmChange, this));
 
   // The extra cel must be null, because if it's not null, it means
   // that someone else is using it (e.g. the editor brush preview),
   // and its owner could destroy our new "extra cel".
   ASSERT(!m_document->getExtraCel());
-
   redrawExtraImage();
 
-  m_initialMask = new Mask(*m_document->mask());
-  m_currentMask = new Mask(*m_document->mask());
-
-  m_rotAlgoConn =
-    Preferences::instance().selection.rotationAlgorithm.AfterChange.connect(
-      Bind<void>(&PixelsMovement::onRotationAlgorithmChange, this));
+  redrawCurrentMask();
+  updateDocumentMask();
+  update_screen_for_document(m_document);
 }
 
 PixelsMovement::~PixelsMovement()
@@ -165,16 +170,6 @@ void PixelsMovement::catchImageAgain(const gfx::Point& pos, HandleType handle)
     m_document->generateMaskBoundaries(&emptyMask);
     update_screen_for_document(m_document);
   }
-}
-
-void PixelsMovement::maskImage(const Image* image)
-{
-  m_currentMask->replace(m_currentData.bounds());
-  m_initialMask->copyFrom(m_currentMask);
-
-  updateDocumentMask();
-
-  update_screen_for_document(m_document);
 }
 
 void PixelsMovement::moveImage(const gfx::Point& pos, MoveModifier moveModifier)
@@ -412,29 +407,35 @@ void PixelsMovement::moveImage(const gfx::Point& pos, MoveModifier moveModifier)
   }
 }
 
-Image* PixelsMovement::getDraggedImageCopy(gfx::Point& origin)
+void PixelsMovement::getDraggedImageCopy(base::UniquePtr<Image>& outputImage,
+                                         base::UniquePtr<Mask>& outputMask)
 {
-  gfx::Transformation::Corners corners;
-  m_currentData.transformBox(corners);
+  gfx::Rect bounds = m_currentData.transformedBounds();
+  base::UniquePtr<Image> image(Image::create(m_sprite->pixelFormat(), bounds.w, bounds.h));
 
-  gfx::Point leftTop(corners[0]);
-  gfx::Point rightBottom(corners[0]);
-  for (size_t i=1; i<corners.size(); ++i) {
-    if (leftTop.x > corners[i].x) leftTop.x = (int)corners[i].x;
-    if (leftTop.y > corners[i].y) leftTop.y = (int)corners[i].y;
-    if (rightBottom.x < corners[i].x) rightBottom.x = (int)corners[i].x;
-    if (rightBottom.y < corners[i].y) rightBottom.y = (int)corners[i].y;
+  drawImage(image, bounds.getOrigin(), false);
+
+  // Draw mask without shrinking it, so the mask size is equal to the
+  // "image" render.
+  base::UniquePtr<Mask> mask(new Mask);
+  drawMask(mask, false);
+
+  // Now we can shrink and crop the image.
+  gfx::Rect oldMaskBounds = mask->bounds();
+  mask->shrink();
+  gfx::Rect newMaskBounds = mask->bounds();
+  if (newMaskBounds != oldMaskBounds) {
+    newMaskBounds.x -= oldMaskBounds.x;
+    newMaskBounds.y -= oldMaskBounds.y;
+    image.reset(crop_image(image,
+                           newMaskBounds.x,
+                           newMaskBounds.y,
+                           newMaskBounds.w,
+                           newMaskBounds.h, 0));
   }
 
-  int width = rightBottom.x - leftTop.x;
-  int height = rightBottom.y - leftTop.y;
-  base::UniquePtr<Image> image(Image::create(m_sprite->pixelFormat(), width, height));
-
-  drawImage(image, leftTop, false);
-
-  origin = leftTop;
-
-  return image.release();
+  outputImage.reset(image.release());
+  outputMask.reset(mask.release());
 }
 
 void PixelsMovement::stampImage()
@@ -572,13 +573,13 @@ gfx::Size PixelsMovement::getInitialImageSize() const
   return m_initialData.bounds().getSize();
 }
 
-void PixelsMovement::setMaskColor(color_t mask_color)
+void PixelsMovement::setMaskColor(bool opaque, color_t mask_color)
 {
   ContextWriter writer(m_reader, 5000);
-
+  m_opaque = opaque;
   m_maskColor = mask_color;
-
   redrawExtraImage();
+
   update_screen_for_document(m_document);
 }
 
@@ -601,19 +602,7 @@ void PixelsMovement::redrawExtraImage()
 
 void PixelsMovement::redrawCurrentMask()
 {
-  gfx::Transformation::Corners corners;
-  m_currentData.transformBox(corners);
-
-  // Transform mask
-
-  gfx::Rect bounds = m_currentData.transformedBounds();
-  m_currentMask->replace(bounds);
-  m_currentMask->freeze();
-  clear_image(m_currentMask->bitmap(), 0);
-  drawParallelogram(m_currentMask->bitmap(), m_initialMask->bitmap(),
-                    corners, bounds.getOrigin());
-
-  m_currentMask->unfreeze();
+  drawMask(m_currentMask, true);
 }
 
 void PixelsMovement::drawImage(doc::Image* dst, const gfx::Point& pt, bool renderOriginalLayer)
@@ -622,10 +611,9 @@ void PixelsMovement::drawImage(doc::Image* dst, const gfx::Point& pt, bool rende
 
   gfx::Transformation::Corners corners;
   m_currentData.transformBox(corners);
+  gfx::Rect bounds = corners.bounds();
 
   dst->setMaskColor(m_sprite->transparentColor());
-
-  gfx::Rect bounds = m_currentData.transformedBounds();
   dst->clear(dst->maskColor());
 
   if (renderOriginalLayer)
@@ -634,11 +622,38 @@ void PixelsMovement::drawImage(doc::Image* dst, const gfx::Point& pt, bool rende
       gfx::Clip(bounds.x-pt.x, bounds.y-pt.y, bounds),
       BlendMode::SRC);
 
-  m_originalImage->setMaskColor(m_maskColor);
-  drawParallelogram(dst, m_originalImage, corners, pt);
+  color_t maskColor = m_maskColor;
+  if (m_opaque) {
+    if (m_originalImage->pixelFormat() == IMAGE_INDEXED)
+      maskColor = -1;
+    else
+      maskColor = 0;
+  }
+  m_originalImage->setMaskColor(maskColor);
+
+  drawParallelogram(dst, m_originalImage, m_initialMask, corners, pt);
 }
 
-void PixelsMovement::drawParallelogram(doc::Image* dst, doc::Image* src,
+void PixelsMovement::drawMask(doc::Mask* mask, bool shrink)
+{
+  gfx::Transformation::Corners corners;
+  m_currentData.transformBox(corners);
+  gfx::Rect bounds = corners.bounds();
+
+  mask->replace(bounds);
+  if (shrink)
+    mask->freeze();
+  clear_image(mask->bitmap(), 0);
+  drawParallelogram(mask->bitmap(),
+                    m_initialMask->bitmap(),
+                    nullptr,
+                    corners, bounds.getOrigin());
+  if (shrink)
+    mask->unfreeze();
+}
+
+void PixelsMovement::drawParallelogram(
+  doc::Image* dst, const doc::Image* src, const doc::Mask* mask,
   const gfx::Transformation::Corners& corners,
   const gfx::Point& leftTop)
 {
@@ -658,7 +673,8 @@ retry:;      // In case that we don't have enough memory for RotSprite
   switch (rotAlgo) {
 
     case tools::RotationAlgorithm::FAST:
-      doc::algorithm::parallelogram(dst, src,
+      doc::algorithm::parallelogram(
+        dst, src, (mask ? mask->bitmap(): nullptr),
         int(corners.leftTop().x-leftTop.x),
         int(corners.leftTop().y-leftTop.y),
         int(corners.rightTop().x-leftTop.x),
@@ -671,7 +687,8 @@ retry:;      // In case that we don't have enough memory for RotSprite
 
     case tools::RotationAlgorithm::ROTSPRITE:
       try {
-        doc::algorithm::rotsprite_image(dst, src,
+        doc::algorithm::rotsprite_image(
+          dst, src, (mask ? mask->bitmap(): nullptr),
           int(corners.leftTop().x-leftTop.x),
           int(corners.leftTop().y-leftTop.y),
           int(corners.rightTop().x-leftTop.x),

data/pref.xml
@@ -110,6 +110,7 @@
     </section>
     <section id="selection">
       <option id="mode" type="app::tools::SelectionMode" default="app::tools::SelectionMode::DEFAULT" />
+      <option id="opaque" type="bool" default="false" />
       <option id="transparent_color" type="app::Color" />
       <option id="rotation_algorithm" type="app::tools::RotationAlgorithm" default="app::tools::RotationAlgorithm::DEFAULT" />
     </section>

data/skins/default/skin.xml
@@ -375,6 +375,8 @@
     <part id="ink_default"                      x="144" y="144" w="16" h="16" />
     <part id="ink_composite"                    x="160" y="144" w="16" h="16" />
     <part id="ink_lock_alpha"                   x="176" y="144" w="16" h="16" />
+    <part id="selection_opaque"                 x="208" y="176" w="16" h="10" />
+    <part id="selection_masked"                 x="224" y="176" w="16" h="10" />
   </parts>
 
   <stylesheet>

src/app/thumbnail_generator.cpp
@@ -90,7 +90,9 @@ private:
         // Stretch the 'image'
         m_thumbnail.reset(Image::create(image->pixelFormat(), thumb_w, thumb_h));
         clear_image(m_thumbnail, 0);
-        algorithm::scale_image(m_thumbnail, image, 0, 0, thumb_w, thumb_h);
+        algorithm::scale_image(m_thumbnail, image,
+                               0, 0, thumb_w, thumb_h,
+                               0, 0, image->width(), image->height());
       }
 
       // Close file

src/app/ui/context_bar.cpp
@@ -477,18 +477,72 @@ protected:
   }
 };
 
-
-class ContextBar::TransparentColorField : public ColorButton
-{
+class ContextBar::TransparentColorField : public HBox {
 public:
-  TransparentColorField() : ColorButton(app::Color::fromMask(), IMAGE_RGB) {
-    Change.connect(Bind<void>(&TransparentColorField::onChange, this));
+  TransparentColorField(ContextBar* owner)
+    : m_icon(1)
+    , m_maskColor(app::Color::fromMask(), IMAGE_RGB)
+    , m_owner(owner) {
+    addChild(&m_icon);
+    addChild(&m_maskColor);
+
+    m_icon.addItem(static_cast<SkinTheme*>(getTheme())->get_part(PART_SELECTION_OPAQUE));
+    gfx::Size sz = m_icon.getItem(0)->getPreferredSize();
+    sz.w += 2*guiscale();
+    m_icon.getItem(0)->setMinSize(sz);
+    setOpaque(Preferences::instance().selection.opaque());
+
+    m_icon.ItemChange.connect(Bind<void>(&TransparentColorField::onPopup, this));
+    m_maskColor.Change.connect(Bind<void>(&TransparentColorField::onChangeColor, this));
   }
 
-protected:
-  void onChange() {
-    Preferences::instance().selection.transparentColor(getColor());
+private:
+
+  void onPopup() {
+    gfx::Rect bounds = getBounds();
+
+    Menu menu;
+    MenuItem
+      opaque("Opaque"),
+      masked("Transparent");
+    menu.addChild(&opaque);
+    menu.addChild(&masked);
+
+    if (Preferences::instance().selection.opaque())
+      opaque.setSelected(true);
+    else
+      masked.setSelected(true);
+
+    opaque.Click.connect(Bind<void>(&TransparentColorField::setOpaque, this, true));
+    masked.Click.connect(Bind<void>(&TransparentColorField::setOpaque, this, false));
+
+    menu.showPopup(gfx::Point(bounds.x, bounds.y+bounds.h));
   }
+
+  void onChangeColor() {
+    Preferences::instance().selection.transparentColor(
+      m_maskColor.getColor());
+  }
+
+  void setOpaque(bool opaque) {
+    int part = (opaque ? PART_SELECTION_OPAQUE: PART_SELECTION_MASKED);
+    m_icon.getItem(0)->setIcon(
+      static_cast<SkinTheme*>(getTheme())->get_part(part));
+
+    m_maskColor.setVisible(!opaque);
+    Preferences::instance().selection.opaque(opaque);
+    if (!opaque) {
+      Preferences::instance().selection.transparentColor(
+        m_maskColor.getColor());
+    }
+
+    if (m_owner)
+      m_owner->layout();
+  }
+
+  ButtonSet m_icon;
+  ColorButton m_maskColor;
+  ContextBar* m_owner;
 };
 
 class ContextBar::RotAlgorithmField : public ComboBox
@@ -860,7 +914,7 @@ ContextBar::ContextBar()
   addChild(m_selectionOptionsBox = new HBox());
   m_selectionOptionsBox->addChild(m_dropPixels = new DropPixelsField());
   m_selectionOptionsBox->addChild(m_selectionMode = new SelectionModeField);
-  m_selectionOptionsBox->addChild(m_transparentColor = new TransparentColorField);
+  m_selectionOptionsBox->addChild(m_transparentColor = new TransparentColorField(this));
   m_selectionOptionsBox->addChild(m_rotAlgo = new RotAlgorithmField());
 
   addChild(m_brushType = new BrushTypeField(this));

src/app/ui/editor/editor.cpp
@@ -1417,8 +1417,11 @@ void Editor::setZoomAndCenterInMouse(Zoom zoom,
   }
 }
 
-void Editor::pasteImage(const Image* image, const gfx::Point& pos)
+void Editor::pasteImage(const Image* image, const Mask* mask)
 {
+  ASSERT(image);
+  ASSERT(mask);
+
   // Change to a selection tool: it's necessary for PixelsMovement
   // which will use the extra cel for transformation preview, and is
   // not compatible with the drawing cursor preview which overwrite
@@ -1433,8 +1436,8 @@ void Editor::pasteImage(const Image* image, const gfx::Point& pos)
   Sprite* sprite = this->sprite();
 
   // Check bounds where the image will be pasted.
-  int x = pos.x;
-  int y = pos.y;
+  int x = mask->bounds().x;
+  int y = mask->bounds().y;
   {
     // Then we check if the image will be visible by the user.
     Rect visibleBounds = getVisibleSpriteBounds().shrink(4*ui::guiscale());
@@ -1450,12 +1453,12 @@ void Editor::pasteImage(const Image* image, const gfx::Point& pos)
   // pasted image.
   m_brushPreview.hide();
 
+  Mask mask2(*mask);
+  mask2.setOrigin(x, y);
+
   PixelsMovementPtr pixelsMovement(
     new PixelsMovement(UIContext::instance(),
-      getSite(), image, gfx::Point(x, y), "Paste"));
-
-  // Select the pasted image so the user can move it and transform it.
-  pixelsMovement->maskImage(image);
+                       getSite(), image, &mask2, "Paste"));
 
   setState(EditorStatePtr(new MovingPixelsState(this, NULL, pixelsMovement, NoHandle)));
 }

src/app/ui/editor/editor.h
@@ -178,7 +178,7 @@ namespace app {
     void setZoomAndCenterInMouse(render::Zoom zoom,
       const gfx::Point& mousePos, ZoomBehavior zoomBehavior);
 
-    void pasteImage(const Image* image, const gfx::Point& pos);
+    void pasteImage(const Image* image, const Mask* mask);
 
     void startSelectionTransformation(const gfx::Point& move);
 

commit 66c349958d0850cab4e48d5d42f4b7e58073d909

Fix bug undoing/redoing pixel movements with several mask movements
If we call updateDocumentMask() in this point, we'll leave a
sequence of cmds: SetMask(old mask, new mask) -> ClearMask(new mask)
And we need the opposite: ClearMask(old mask) -> SetMask(old mask, new mask)

This bug was introduced in 35229e99a6196d71b90ef2762abb32b0a164f594

src/app/ui/editor/pixels_movement.cpp
@@ -88,9 +88,7 @@ PixelsMovement::PixelsMovement(
   // and its owner could destroy our new "extra cel".
   ASSERT(!m_document->getExtraCel());
   redrawExtraImage();
-
   redrawCurrentMask();
-  updateDocumentMask();
 }
 
 PixelsMovement::~PixelsMovement()