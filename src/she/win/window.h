evilguy50/aseprite

commit 119c87873f4660e4553aecc0d4ad2ae59e016fa5

Add she::PointerType::Cursor to identify puck like devices

src/she/win/window.h
@@ -624,10 +624,22 @@ namespace she {
           PACKET packet;
 
           if (api.packet(ctx, serial, &packet)) {
-            if (packet.pkCursor == 2 || packet.pkCursor == 5)
-              m_pointerType = PointerType::Eraser;
-            else
-              m_pointerType = PointerType::Pen;
+            switch (packet.pkCursor) {
+              case 0:
+              case 3:
+                m_pointerType = PointerType::Cursor;
+                break;
+              case 1:
+              case 4:
+                m_pointerType = PointerType::Pen;
+                break;
+              case 2
+              case 5:
+                m_pointerType = PointerType::Eraser;
+                break;
+              default:
+                m_pointerType = PointerType::Unknown;
+                break;
           }
           else
             m_pointerType = PointerType::Unknown;

src/she/pointer_type.h
@@ -10,12 +10,14 @@
 
 namespace she {
 
+  // Source of a mouse like event
   enum class PointerType {
     Unknown,
-    Mouse,
-    Multitouch,
-    Pen,
-    Eraser
+    Mouse,                      // A regular mouse
+    Multitouch,                 // Trackpad/multitouch surface
+    Pen,                        // Stylus pen
+    Cursor,                     // Puck like device
+    Eraser                      // Eraser end of a stylus pen
   };
 
 } // namespace she

commit 213cb8b2df6aea1344ea26048d6dd3b6ed4bc954

Fix compilation error introduced in 119c87873f4660e4553aecc0d4ad2ae59e016fa5

src/she/win/window.h
@@ -633,13 +633,14 @@ namespace she {
               case 4:
                 m_pointerType = PointerType::Pen;
                 break;
-              case 2
+              case 2:
               case 5:
                 m_pointerType = PointerType::Eraser;
                 break;
               default:
                 m_pointerType = PointerType::Unknown;
                 break;
+            }
           }
           else
             m_pointerType = PointerType::Unknown;