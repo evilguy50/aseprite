evilguy50/aseprite

commit 00099390daf9d24cae6910e3c67d43e15b7cc542

Fix several keyboard issues deadling with special characters/dead keys
This change adds support to write text with dead keys, and assign
keyboard shortcuts to special key combinations with Unicode characters
on macOS and Windows.

Fix #1083, close #796

src/ui/entry.cpp
@@ -14,6 +14,7 @@
 #include "base/string.h"
 #include "clip/clip.h"
 #include "she/font.h"
+#include "she/system.h"
 #include "ui/manager.h"
 #include "ui/menu.h"
 #include "ui/message.h"
@@ -41,6 +42,7 @@ Entry::Entry(std::size_t maxsize, const char* format, ...)
   , m_password(false)
   , m_recent_focused(false)
   , m_lock_selection(false)
+  , m_translate_dead_keys(true)
 {
   enableFlags(CTRL_RIGHT_CLICK);
 
@@ -165,6 +167,11 @@ void Entry::setSuffix(const std::string& suffix)
   invalidate();
 }
 
+void Entry::setTranslateDeadKeys(bool state)
+{
+  m_translate_dead_keys = state;
+}
+
 void Entry::getEntryThemeInfo(int* scroll, int* caret, int* state,
                               int* selbeg, int* selend)
 {
@@ -213,6 +220,10 @@ bool Entry::onProcessMessage(Message* msg)
         selectAllText();
         m_recent_focused = true;
       }
+
+      // Start processing dead keys
+      if (m_translate_dead_keys)
+        she::instance()->setTranslateDeadKeys(true);
       break;
 
     case kFocusLeaveMessage:
@@ -224,6 +235,10 @@ bool Entry::onProcessMessage(Message* msg)
         deselectText();
 
       m_recent_focused = false;
+
+      // Stop processing dead keys
+      if (m_translate_dead_keys)
+        she::instance()->setTranslateDeadKeys(false);
       break;
 
     case kKeyDownMessage:
@@ -300,20 +315,24 @@ bool Entry::onProcessMessage(Message* msg)
               }
             }
             else if (manager()->isFocusMovementKey(msg)) {
-              break;
-            }
-            else if (keymsg->unicodeChar() >= 32) {
-              // Ctrl and Alt must be unpressed to insert a character
-              // in the text-field.
-              if ((msg->modifiers() & (kKeyCtrlModifier | kKeyAltModifier)) == 0) {
-                cmd = EntryCmd::InsertChar;
-              }
+              return Widget::onProcessMessage(msg);
             }
             break;
         }
 
-        if (cmd == EntryCmd::NoOp)
+        if (cmd == EntryCmd::NoOp) {
+          if (keymsg->unicodeChar() >= 32) {
+            executeCmd(EntryCmd::InsertChar, keymsg->unicodeChar(),
+                       (msg->shiftPressed()) ? true: false);
+
+            // Select dead-key
+            if (keymsg->isDeadKey()) {
+              selectText(m_caret-1, m_caret);
+            }
+            return true;
+          }
           break;
+        }
 
         executeCmd(cmd, keymsg->unicodeChar(),
                    (msg->shiftPressed()) ? true: false);

src/app/modules/gui.cpp
@@ -383,6 +383,8 @@ bool CustomizedGuiManager::onProcessMessage(Message* msg)
       break;
 
     case kKeyDownMessage: {
+      auto keymsg = static_cast<KeyMessage*>(msg);
+
 #ifdef _DEBUG
       // Ctrl+Shift+Q generates a crash (useful to test the anticrash feature)
       if (msg->ctrlPressed() &&
@@ -396,7 +398,7 @@ bool CustomizedGuiManager::onProcessMessage(Message* msg)
       // Ctrl+Shift+R recover active sprite from the backup store
       if (msg->ctrlPressed() &&
           msg->shiftPressed() &&
-          static_cast<KeyMessage*>(msg)->scancode() == kKeyR &&
+          keymsg->scancode() == kKeyR &&
           App::instance()->dataRecovery() &&
           App::instance()->dataRecovery()->activeSession() &&
           current_editor &&

src/app/ui/select_accelerator.cpp
@@ -23,6 +23,7 @@ using namespace ui;
 class SelectAccelerator::KeyField : public ui::Entry {
 public:
   KeyField(const Accelerator& accel) : ui::Entry(256, "") {
+    setTranslateDeadKeys(false);
     setExpansive(true);
     setFocusMagnet(true);
     setAccel(accel);
@@ -38,9 +39,13 @@ public:
 protected:
   bool onProcessMessage(Message* msg) override {
     switch (msg->type()) {
+
       case kKeyDownMessage:
         if (hasFocus() && !isReadOnly()) {
           KeyMessage* keymsg = static_cast<KeyMessage*>(msg);
+          if (!keymsg->scancode() && keymsg->unicodeChar() < 32)
+            break;
+
           KeyModifiers modifiers = keymsg->modifiers();
 
           if (keymsg->scancode() == kKeySpace)

src/she/alleg4/key_poller.cpp
@@ -1,5 +1,5 @@
 // SHE library
-// Copyright (C) 2012-2015  David Capello
+// Copyright (C) 2012-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -21,14 +21,12 @@ int key_repeated[KEY_MAX];
 int she_keyboard_ucallback(int unicode_char, int* scancode)
 {
   int c = ((*scancode) & 0x7f);
-  Event ev;
 
+  Event ev;
   ev.setType(Event::KeyDown);
   ev.setScancode(static_cast<KeyScancode>(c));
   if (unicode_char > 0)
     ev.setUnicodeChar(unicode_char);
-  else
-    ev.setUnicodeChar(::scancode_to_ascii(c));
   ev.setRepeat(key_repeated[c]++);
   queue_event(ev);
 

src/she/alleg4/she.cpp
@@ -206,6 +206,10 @@ public:
     return sur;
   }
 
+  void setTranslateDeadKeys(bool state) override {
+    // Do nothing
+  }
+
 };
 
 System* create_system() {
@@ -245,6 +249,14 @@ bool is_key_pressed(KeyScancode scancode)
   }
 #endif
   return key[scancode] ? true: false;
+}
+
+int get_unicode_from_scancode(KeyScancode scancode)
+{
+  if (is_key_pressed(scancode))
+    return scancode_to_ascii(scancode);
+  else
+    return false;
 }
 
 void clear_keyboard_buffer()

src/she/event.h
@@ -56,6 +56,7 @@ namespace she {
               m_scancode(kKeyNil),
               m_modifiers(kKeyUninitializedModifier),
               m_unicodeChar(0),
+              m_isDead(false),
               m_repeat(0),
               m_preciseWheel(false),
               m_pointerType(PointerType::Unknown),
@@ -70,6 +71,7 @@ namespace she {
     KeyScancode scancode() const { return m_scancode; }
     KeyModifiers modifiers() const { return m_modifiers; }
     int unicodeChar() const { return m_unicodeChar; }
+    bool isDeadKey() const { return m_isDead; }
     int repeat() const { return m_repeat; }
     gfx::Point position() const { return m_position; }
     gfx::Point wheelDelta() const { return m_wheelDelta; }
@@ -92,6 +94,7 @@ namespace she {
     void setScancode(KeyScancode scancode) { m_scancode = scancode; }
     void setModifiers(KeyModifiers modifiers) { m_modifiers = modifiers; }
     void setUnicodeChar(int unicodeChar) { m_unicodeChar = unicodeChar; }
+    void setDeadKey(bool state) { m_isDead = state; }
     void setRepeat(int repeat) { m_repeat = repeat; }
     void setPosition(const gfx::Point& pos) { m_position = pos; }
     void setWheelDelta(const gfx::Point& delta) { m_wheelDelta = delta; }
@@ -108,6 +111,7 @@ namespace she {
     KeyScancode m_scancode;
     KeyModifiers m_modifiers;
     int m_unicodeChar;
+    bool m_isDead;
     int m_repeat; // repeat=0 means the first time the key is pressed
     gfx::Point m_position;
     gfx::Point m_wheelDelta;

src/she/keys.h
@@ -1,5 +1,5 @@
 // SHE library
-// Copyright (C) 2012-2013, 2015  David Capello
+// Copyright (C) 2012-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -156,9 +156,17 @@ namespace she {
     kKeyScancodes    = 127
   };
 
-  // Deprecated API, use modifiers in she::Event
-  // TODO mark these functions as deprecated
+  // TODO move these functions to she::System
+
+  // Returns true if the the given scancode key is pressed/actived.
   bool is_key_pressed(KeyScancode scancode);
+
+  // Returns the latest unicode character that activated the given
+  // scancode.
+  int get_unicode_from_scancode(KeyScancode scancode);
+
+  // Clears the keyboard buffer (used only in the Allegro port).
+  // TODO (deprecated)
   void clear_keyboard_buffer();
 
 } // namespace she

src/she/osx/view.h
@@ -53,6 +53,8 @@
 - (void)updateCurrentCursor;
 - (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender;
 - (BOOL)performDragOperation:(id<NSDraggingInfo>)sender;
+- (void)doCommandBySelector:(SEL)selector;
+- (void)setTranslateDeadKeys:(BOOL)state;
 @end
 
 #endif

commit 2598dde9aeb02ac836873724f5a16367626cd286

Fix 1...6 keys to go to a specific frame on Windows
These keys where captured by the zoom command (gui Manager) first.
This regression was introduced in 00099390daf9d24cae6910e3c67d43e15b7cc542

src/ui/entry.cpp
@@ -329,9 +329,8 @@ bool Entry::onProcessMessage(Message* msg)
             if (keymsg->isDeadKey()) {
               selectText(m_caret-1, m_caret);
             }
-            return true;
           }
-          break;
+          return true;
         }
 
         executeCmd(cmd, keymsg->unicodeChar(),