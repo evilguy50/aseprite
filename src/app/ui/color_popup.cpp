evilguy50/aseprite

commit 52375f3aa9f7c2b7fa0186be697428711b59ae1a

Fix clicking "Index" and "RGB" button on color selector
To avoid adding the m_titleLabel to the ColorPopup/PopupWindowPin().

src/app/ui/color_popup.cpp
@@ -48,7 +48,7 @@ enum {
 };
 
 ColorPopup::ColorPopup(bool canPin)
-  : PopupWindowPin("Color Selector",
+  : PopupWindowPin(std::string(),
                    ClickBehavior::CloseOnClickInOtherWindow,
                    canPin)
   , m_vbox(VERTICAL)

commit 9b87dae1bd84d881ef6bef6976d0eebae5374bb4

Show close button on ColorPopup
Fix regression introduced in 52375f3aa9f7c2b7fa0186be697428711b59ae1a

src/app/ui/color_popup.cpp
@@ -47,7 +47,7 @@ enum {
 };
 
 ColorPopup::ColorPopup(bool canPin)
-  : PopupWindowPin(std::string(),
+  : PopupWindowPin(" ", // Non-empty to create title-bar and close button
                    ClickBehavior::CloseOnClickInOtherWindow,
                    canPin)
   , m_vbox(VERTICAL)
@@ -98,6 +98,7 @@ ColorPopup::ColorPopup(bool canPin)
       m_topBox.addChild(vbox);
     }
   }
+  setText("");                  // To remove title
 
   m_vbox.addChild(&m_topBox);
   m_vbox.addChild(&m_colorPaletteContainer);