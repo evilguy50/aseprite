evilguy50/aseprite

commit 767897d6650cf5ea625611569d6123715d599d0d

Set DIRTY flag on Widgets without going to all parents

src/ui/widget.cpp
@@ -1,4 +1,5 @@
 // Aseprite UI Library
+// Copyright (C) 2018  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -45,14 +46,6 @@
 namespace ui {
 
 using namespace gfx;
-
-static inline void mark_dirty_flag(Widget* widget)
-{
-  while (widget) {
-    widget->enableFlags(DIRTY);
-    widget = widget->parent();
-  }
-}
 
 WidgetType register_widget_type()
 {
@@ -1111,7 +1104,7 @@ void Widget::invalidate()
     m_updateRegion.clear();
     getDrawableRegion(m_updateRegion, kCutTopWindows);
 
-    mark_dirty_flag(this);
+    setDirtyFlag();
 
     for (auto child : m_children)
       child->invalidate();
@@ -1497,7 +1490,7 @@ void Widget::onInvalidateRegion(const Region& region)
     }
     reg1.createSubtraction(region, m_updateRegion);
 
-    mark_dirty_flag(this);
+    setDirtyFlag();
 
     for (auto child : m_children)
       child->invalidateRegion(reg1);
@@ -1613,6 +1606,21 @@ void Widget::offsetWidgets(int dx, int dy)
 
   for (auto child : m_children)
     child->offsetWidgets(dx, dy);
+}
+
+void Widget::setDirtyFlag()
+{
+  Widget* widget = this;
+  while (widget && !widget->hasFlags(DIRTY)) {
+    widget->enableFlags(DIRTY);
+    widget = widget->parent();
+  }
+#if _DEBUG // Check that all parents has the DIRTY flag
+  while (widget) {
+    ASSERT(widget->hasFlags(DIRTY));
+    widget = widget->parent();
+  }
+#endif
 }
 
 } // namespace ui

src/ui/widget.h
@@ -1,4 +1,5 @@
 // Aseprite UI Library
+// Copyright (C) 2018  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -399,6 +400,7 @@ namespace ui {
                const bool isBg);
     bool paintEvent(Graphics* graphics,
                     const bool isBg);
+    void setDirtyFlag();
 
     WidgetType m_type;           // Widget's type
     std::string m_id;            // Widget's id

commit 15f01be2959c2b65a4829bd047a3d4ef3dfd8ba8

Fix regression introduced in 767897d6650cf5ea625611569d6123715d599d0d
In some cases we have to enable the DIRTY flag in the hierarchy.
Reproducible case: right-click a tab with an image, open in a folder,
then right-click the tab again.

src/ui/widget.cpp
@@ -1611,16 +1611,10 @@ void Widget::offsetWidgets(int dx, int dy)
 void Widget::setDirtyFlag()
 {
   Widget* widget = this;
-  while (widget && !widget->hasFlags(DIRTY)) {
+  while (widget) {
     widget->enableFlags(DIRTY);
     widget = widget->parent();
   }
-#if _DEBUG // Check that all parents has the DIRTY flag
-  while (widget) {
-    ASSERT(widget->hasFlags(DIRTY));
-    widget = widget->parent();
-  }
-#endif
 }
 
 } // namespace ui