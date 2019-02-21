evilguy50/aseprite

commit 6a88713213403179fb04938a99085e60bad7e873

Avoid validating hidden areas while we're in the tool loop for tools like line/square/ellipse, etc.

src/app/ui/editor/tool_loop_impl.cpp
@@ -260,6 +260,10 @@ public:
   tools::Symmetry* getSymmetry() override { return m_symmetry.get(); }
   doc::Remap* getShadingRemap() override { return m_shadingRemap.get(); }
 
+  void limitDirtyAreaToViewport(gfx::Region& rgn) override {
+    rgn &= gfx::Region(m_editor->getVisibleSpriteBounds().inflate(1, 1));
+  }
+
   void updateDirtyArea(const gfx::Region& dirtyArea) override {
     // This is necessary here so the "on sprite crosshair" is hidden,
     // we update screen pixels with the new sprite, and then we show

src/app/tools/tool_loop.h
@@ -222,6 +222,8 @@ namespace app {
       // Returns true if the loop was canceled by the user
       virtual bool isCanceled() = 0;
 
+      virtual void limitDirtyAreaToViewport(gfx::Region& rgn) = 0;
+
       // Redraws the dirty area.
       virtual void updateDirtyArea(const gfx::Region& dirtyArea) = 0;
 

src/app/tools/tool_loop_manager.cpp
@@ -111,7 +111,8 @@ bool ToolLoopManager::releaseButton(const Pointer& pointer)
 
   bool res = m_toolLoop->getController()->releaseButton(m_stroke, spritePoint);
 
-  if (!res && (m_toolLoop->getInk()->isSelection() ||
+  if (!res && (m_toolLoop->getTracePolicy() == TracePolicy::Last ||
+               m_toolLoop->getInk()->isSelection() ||
                m_toolLoop->getInk()->isSlice() ||
                m_toolLoop->getFilled())) {
     m_toolLoop->getInk()->setFinalStep(m_toolLoop, true);
@@ -165,6 +166,13 @@ void ToolLoopManager::doLoopStep(bool lastStep)
 
   calculateDirtyArea(strokes);
 
+  // If we are not in the last step (when the mouse button is
+  // released) we are only showing a preview of the tool, so we can
+  // limit the dirty area to the visible viewport bounds. In this way
+  // the area using in validateoDstImage() can be a lot smaller.
+  if (m_toolLoop->getTracePolicy() == TracePolicy::Last && !lastStep)
+    m_toolLoop->limitDirtyAreaToViewport(m_dirtyArea);
+
   // Validate source image area.
   if (m_toolLoop->getInk()->needsSpecialSourceArea()) {
     gfx::Region srcArea;
@@ -177,10 +185,10 @@ void ToolLoopManager::doLoopStep(bool lastStep)
 
   m_toolLoop->getInk()->prepareForStrokes(m_toolLoop, strokes);
 
-  // Invalidate destionation image areas.
+  // Invalidate destination image area.
   if (m_toolLoop->getTracePolicy() == TracePolicy::Last) {
     // Copy source to destination (reset the previous trace). Useful
-    // for tools like Line and Ellipse (we kept the last trace only).
+    // for tools like Line and Ellipse (we keep the last trace only).
     m_toolLoop->invalidateDstImage();
   }
   else if (m_toolLoop->getTracePolicy() == TracePolicy::AccumulateUpdateLast) {

commit 5d5287bcb653cbe95acf3995afcd2be2206f9ef9

Show modified regions correctly in ToolLoop on visible areas in non-active editors
Regression introduced in 6a88713213403179fb04938a99085e60bad7e873

src/app/ui/editor/tool_loop_impl.cpp
@@ -40,6 +40,7 @@
 #include "app/ui/main_window.h"
 #include "app/ui/optional_alert.h"
 #include "app/ui/status_bar.h"
+#include "app/ui_context.h"
 #include "app/util/expand_cel_canvas.h"
 #include "doc/brush.h"
 #include "doc/cel.h"
@@ -261,7 +262,19 @@ public:
   doc::Remap* getShadingRemap() override { return m_shadingRemap.get(); }
 
   void limitDirtyAreaToViewport(gfx::Region& rgn) override {
-    rgn &= gfx::Region(m_editor->getVisibleSpriteBounds().inflate(1, 1));
+    // Visible region (on the screen) of the all editors showing the
+    // given document.
+    gfx::Region allVisibleRgn;
+
+    // TODO use the context given to the ToolLoopImpl ctor
+    for (auto e : UIContext::instance()->getAllEditorsIncludingPreview(m_document)) {
+      gfx::Region viewportRegion;
+      e->getDrawableRegion(viewportRegion, Widget::kCutTopWindows);
+      for (auto rc : viewportRegion)
+        allVisibleRgn |= gfx::Region(e->screenToEditor(rc).inflate(1, 1));
+    }
+
+    rgn &= allVisibleRgn;
   }
 
   void updateDirtyArea(const gfx::Region& dirtyArea) override {