evilguy50/aseprite

commit f24eb75298c118500ff4428661fe186eefdde2b2

Add --trim-by-grid CLI option

src/app/commands/cmd_crop.cpp
@@ -12,6 +12,7 @@
 #include "app/commands/command.h"
 #include "app/context_access.h"
 #include "app/doc_api.h"
+#include "app/i18n/strings.h"
 #include "app/modules/gui.h"
 #include "app/tx.h"
 #include "app/ui/color_bar.h"
@@ -87,13 +88,28 @@ public:
   AutocropSpriteCommand();
 
 protected:
+  void onLoadParams(const Params& params) override;
   bool onEnabled(Context* context) override;
   void onExecute(Context* context) override;
+  std::string onGetFriendlyName() const override;
+
+private:
+    bool m_byGrid = false;
 };
 
 AutocropSpriteCommand::AutocropSpriteCommand()
   : Command(CommandId::AutocropSprite(), CmdRecordableFlag)
 {
+}
+
+void AutocropSpriteCommand::onLoadParams(const app::Params &params)
+{
+  m_byGrid = false;
+  if (params.has_param("byGrid")) {
+    std::string isByGrid = params.get("byGrid");
+    if (isByGrid == "true")
+      m_byGrid = true;
+  }
 }
 
 bool AutocropSpriteCommand::onEnabled(Context* context)
@@ -108,8 +124,8 @@ void AutocropSpriteCommand::onExecute(Context* context)
   Doc* document(writer.document());
   Sprite* sprite(writer.sprite());
   {
-    Tx tx(writer.context(), "Trim Sprite");
-    document->getApi(tx).trimSprite(sprite);
+    Tx tx(writer.context(), onGetFriendlyName());
+    document->getApi(tx).trimSprite(sprite, m_byGrid);
     tx.commit();
   }
 
@@ -117,6 +133,14 @@ void AutocropSpriteCommand::onExecute(Context* context)
   if (context->isUIAvailable())
     update_screen_for_document(document);
 #endif
+}
+
+std::string AutocropSpriteCommand::onGetFriendlyName() const
+{
+  if (m_byGrid)
+    return "Trim Sprite by Grid";
+  else
+    return "Trim Sprite";
 }
 
 Command* CommandFactory::createCropSpriteCommand()

data/gui.xml
@@ -480,6 +480,10 @@
       <key command="SymmetryMode">
         <param name="orientation" value="horizontal" />
       </key>
+      <key command="AutocropSprite" />
+      <key command="AutocropSprite">
+        <param name="byGrid" value="true" />
+      </key>
     </commands>
 
     <!-- Keyboard shortcuts to select tools -->

src/app/cli/app_options.cpp
@@ -59,6 +59,7 @@ AppOptions::AppOptions(int argc, const char* argv[])
   , m_shapePadding(m_po.add("shape-padding").requiresValue("<value>").description("Add padding between frames"))
   , m_innerPadding(m_po.add("inner-padding").requiresValue("<value>").description("Add padding inside each frame"))
   , m_trim(m_po.add("trim").description("Trim all images before exporting"))
+  , m_trimByGrid(m_po.add("trim-by-grid").description("Trim all images by its correspondent grid boundaries before exporting"))
   , m_crop(m_po.add("crop").requiresValue("x,y,width,height").description("Crop all the images to the given rectangle"))
   , m_slice(m_po.add("slice").requiresValue("<name>").description("Crop the sprite to the given slice area"))
   , m_filenameFormat(m_po.add("filename-format").requiresValue("<fmt>").description("Special format to generate filenames"))

src/app/cli/app_options.h
@@ -73,6 +73,7 @@ public:
   const Option& shapePadding() const { return m_shapePadding; }
   const Option& innerPadding() const { return m_innerPadding; }
   const Option& trim() const { return m_trim; }
+  const Option& trimByGrid() const { return m_trimByGrid; }
   const Option& crop() const { return m_crop; }
   const Option& slice() const { return m_slice; }
   const Option& filenameFormat() const { return m_filenameFormat; }
@@ -132,6 +133,7 @@ private:
   Option& m_shapePadding;
   Option& m_innerPadding;
   Option& m_trim;
+  Option& m_trimByGrid;
   Option& m_crop;
   Option& m_slice;
   Option& m_filenameFormat;

src/app/cli/cli_open_file.cpp
@@ -32,6 +32,7 @@ CliOpenFile::CliOpenFile()
   listSlices = false;
   ignoreEmpty = false;
   trim = false;
+  trimByGrid = false;
   oneFrame = false;
   crop = gfx::Rect();
 }

src/app/cli/cli_open_file.h
@@ -37,6 +37,7 @@ namespace app {
     bool listSlices;
     bool ignoreEmpty;
     bool trim;
+    bool trimByGrid;
     bool oneFrame;
     gfx::Rect crop;
 

src/app/cli/cli_processor.cpp
@@ -271,6 +271,14 @@ void CliProcessor::process(Context* ctx)
           if (m_exporter)
             m_exporter->setTrimCels(true);
         }
+        // --trim-by-grid
+        else if (opt == &m_options.trimByGrid()) {
+          cof.trim = cof.trimByGrid = true;
+          if (m_exporter) {
+            m_exporter->setTrimCels(true);
+            m_exporter->setTrimByGrid(true);
+          }
+        }
         // --crop x,y,width,height
         else if (opt == &m_options.crop()) {
           std::vector<std::string> parts;
@@ -712,8 +720,13 @@ void CliProcessor::saveFile(Context* ctx, const CliOpenFile& cof)
         // don't have sheet .json) Also, we should trim each frame
         // individually (a process that can be done only in
         // FileOp::operate()).
-        if (cof.trim)
-          ctx->executeCommand(trimCommand);
+        if (cof.trim) {
+          Params params;
+          if (cof.trimByGrid) {
+            params.set("byGrid", "true");
+          }
+          ctx->executeCommand(trimCommand, params);
+        }
 
         CliOpenFile itemCof = cof;
         FilenameInfo fnInfo;
@@ -744,6 +757,7 @@ void CliProcessor::saveFile(Context* ctx, const CliOpenFile& cof)
         // Call delegate
         m_delegate->saveFile(ctx, itemCof);
 
+        // for trim or trimByGrid case
         if (cof.trim) {
           ctx->executeCommand(undoCommand);
           clearUndo = true;

src/app/cli/preview_cli_delegate.cpp
@@ -96,7 +96,10 @@ void PreviewCliDelegate::saveFile(Context* ctx, const CliOpenFile& cof)
   }
 
   if (cof.trim) {
-    std::cout << "  - Trim\n";
+    if (cof.trimByGrid)
+      std::cout << "  - Trim by Grid\n";
+    else
+      std::cout << "  - Trim\n";
   }
 
   if (cof.ignoreEmpty) {

commit 5d0115fb7963b1e32c01278d083e010e3fe1bde9

Get AutocropSprite strings from i18n file
After introducing AutocropSpriteCommand::onGetFriendlyName(), we stop
using the automatic commands_AutocropSprite string from en.ini. This
commit fix that regression from f24eb75298c118500ff4428661fe186eefdde2b2

data/strings/en.ini
@@ -208,6 +208,7 @@ AddColor_Foreground = Foreground
 AddColor_Specific = Specific
 AdvancedMode = Advanced Mode
 AutocropSprite = Trim Sprite
+AutocropSprite_ByGrid = Trim Sprite by Grid
 BackgroundFromLayer = Background From Layer
 BrightnessContrast = Adjust Brightness/Contrast
 Cancel = Cancel Current Operation

src/app/commands/cmd_crop.cpp
@@ -138,9 +138,9 @@ void AutocropSpriteCommand::onExecute(Context* context)
 std::string AutocropSpriteCommand::onGetFriendlyName() const
 {
   if (m_byGrid)
-    return "Trim Sprite by Grid";
+    return Strings::commands_AutocropSprite_ByGrid();
   else
-    return "Trim Sprite";
+    return Strings::commands_AutocropSprite();
 }
 
 Command* CommandFactory::createCropSpriteCommand()