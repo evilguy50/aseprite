evilguy50/aseprite

commit ed0f4c3023d8a8b78d04b00177e25a9f5840077d

Don't access Preferences to do color conversion from bg threads
Continuation of 673e1955fa10e8ce643c80f64c439852b543f62a

src/app/app.cpp
@@ -17,6 +17,7 @@
 #include "app/cli/cli_processor.h"
 #include "app/cli/default_cli_delegate.h"
 #include "app/cli/preview_cli_delegate.h"
+#include "app/color_spaces.h"
 #include "app/color_utils.h"
 #include "app/commands/commands.h"
 #include "app/console.h"
@@ -59,11 +60,11 @@
 #include "base/split_string.h"
 #include "doc/sprite.h"
 #include "fmt/format.h"
-#include "render/render.h"
 #include "os/display.h"
 #include "os/error.h"
 #include "os/surface.h"
 #include "os/system.h"
+#include "render/render.h"
 #include "ui/intern.h"
 #include "ui/ui.h"
 
@@ -104,6 +105,9 @@ class App::CoreModules {
 public:
   ConfigModule m_configModule;
   Preferences m_preferences;
+  CoreModules() {
+    initialize_color_spaces();
+  }
 };
 
 class App::LoadLanguage {

src/ui/system.cpp
@@ -362,7 +362,8 @@ void execute_from_ui_thread(std::function<void()>&& f)
 
 bool is_ui_thread()
 {
-  return (main_gui_thread == base::this_thread::native_handle());
+  return (!main_gui_thread ||
+          main_gui_thread == base::this_thread::native_handle());
 }
 
 #ifdef _DEBUG

src/app/color_spaces.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018  Igara Studio S.A.
+// Copyright (C) 2018-2019  Igara Studio S.A.
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -18,6 +18,20 @@
 #include "os/system.h"
 
 namespace app {
+
+// We use this variable to avoid accessing Preferences::instance()
+// from background threads when a color conversion between color
+// spaces must be done.
+static bool g_manage = false;
+
+void initialize_color_spaces()
+{
+  g_manage = Preferences::instance().color.manage();
+  Preferences::instance().color.manage.AfterChange.connect(
+    [](bool manage){
+      g_manage = manage;
+    });
+}
 
 os::ColorSpacePtr get_screen_color_space()
 {
@@ -56,7 +70,7 @@ gfx::ColorSpacePtr get_working_rgb_space_from_preferences()
 
 ConvertCS::ConvertCS()
 {
-  if (Preferences::instance().color.manage()) {
+  if (g_manage) {
     auto srcCS = get_current_color_space();
     auto dstCS = get_screen_color_space();
     if (srcCS && dstCS)
@@ -67,7 +81,7 @@ ConvertCS::ConvertCS()
 ConvertCS::ConvertCS(const os::ColorSpacePtr& srcCS,
                      const os::ColorSpacePtr& dstCS)
 {
-  if (Preferences::instance().color.manage()) {
+  if (g_manage) {
     m_conversion = os::instance()->convertBetweenColorSpace(srcCS, dstCS);
   }
 }

src/app/color_spaces.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018  Igara Studio S.A.
+// Copyright (C) 2018-2019  Igara Studio S.A.
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -19,6 +19,8 @@ namespace doc {
 }
 
 namespace app {
+
+  void initialize_color_spaces();
 
   os::ColorSpacePtr get_screen_color_space();
 

commit 9c6209ef139df7dd5ebef4b613fad81c0ddc02f2

Fix regression introduced in ed0f4c3023d8a8b78d04b00177e25a9f5840077d

src/app/app.cpp
@@ -105,9 +105,6 @@ class App::CoreModules {
 public:
   ConfigModule m_configModule;
   Preferences m_preferences;
-  CoreModules() {
-    initialize_color_spaces();
-  }
 };
 
 class App::LoadLanguage {
@@ -230,6 +227,8 @@ void App::initialize(const AppOptions& options)
       createLogInDesktop = true;
       break;
   }
+
+  initialize_color_spaces();
 
   // Load modules
   m_modules = new Modules(createLogInDesktop, preferences());

src/app/pref/preferences.cpp
@@ -26,9 +26,12 @@ static Preferences* singleton = nullptr;
 // static
 Preferences& Preferences::instance()
 {
+#ifdef _DEBUG
   // Preferences can be used only from the main UI thread. In other
   // case access to std::map<> could crash the program.
-  ui::assert_ui_thread();
+  if (ui::UISystem::instance())
+    ui::assert_ui_thread();
+#endif
 
   ASSERT(singleton);
   return *singleton;

src/ui/system.cpp
@@ -362,8 +362,7 @@ void execute_from_ui_thread(std::function<void()>&& f)
 
 bool is_ui_thread()
 {
-  return (!main_gui_thread ||
-          main_gui_thread == base::this_thread::native_handle());
+  return (main_gui_thread == base::this_thread::native_handle());
 }
 
 #ifdef _DEBUG