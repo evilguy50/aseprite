evilguy50/aseprite

commit d0962eb737ab0f652952982db7b0d68596b80545

Fix losing sprite center when we change zoom
This issue is pretty common when we change the zoom and the cursor is
outside the canvas, we lost the center of the sprite or the canvas
goes more far and far away. I've seen this problem on streamers and
myself using the program. Hoping to do a little of improvement in this
area with this patch.

src/app/ui/editor/editor.cpp
@@ -52,6 +52,7 @@
 #include "app/ui_context.h"
 #include "base/bind.h"
 #include "base/chrono.h"
+#include "base/clamp.h"
 #include "base/convert_to.h"
 #include "doc/conversion_to_surface.h"
 #include "doc/doc.h"
@@ -2187,6 +2188,12 @@ void Editor::setZoomAndCenterInMouse(const Zoom& zoom,
       screenPos = mousePos;
       break;
   }
+
+  // Limit zooming screen position to the visible sprite bounds
+  gfx::Rect visibleBounds = editorToScreen(getVisibleSpriteBounds());
+  screenPos.x = base::clamp(screenPos.x, visibleBounds.x, visibleBounds.x2()-1);
+  screenPos.y = base::clamp(screenPos.y, visibleBounds.y, visibleBounds.y2()-1);
+
   spritePos = screenToEditor(screenPos);
 
   if (zoomBehavior == ZoomBehavior::MOUSE) {

commit 67db374914eca145e7b9fc426a66f66e03b29b6e

Fix zooming w/tiled mode (fix #2120)
Regression introduced in d0962eb737ab0f652952982db7b0d68596b80545

Bug reported in:
* https://github.com/aseprite/aseprite/issues/2120
* https://community.aseprite.org/t/less-jank-in-tile-view/3659

src/app/ui/editor/editor.cpp
@@ -2279,8 +2279,11 @@ void Editor::setZoomAndCenterInMouse(const Zoom& zoom,
       break;
   }
 
-  // Limit zooming screen position to the visible sprite bounds
-  gfx::Rect visibleBounds = editorToScreen(getVisibleSpriteBounds());
+  // Limit zooming screen position to the visible sprite bounds (we
+  // use canvasSize() because if the tiled mode is enabled, we need
+  // extra space for the zoom)
+  gfx::Rect visibleBounds = editorToScreen(
+    getViewportBounds().createIntersection(gfx::Rect(gfx::Point(0, 0), canvasSize())));
   screenPos.x = base::clamp(screenPos.x, visibleBounds.x, visibleBounds.x2()-1);
   screenPos.y = base::clamp(screenPos.y, visibleBounds.y, visibleBounds.y2()-1);
 