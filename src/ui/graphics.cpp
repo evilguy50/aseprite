evilguy50/aseprite

commit 9a75d01efeaef8db9c61e01dbc9b5aae47c8f515

Improve the File Selector adding new view types: list, small icons, big icons (fix #451)

src/ui/graphics.cpp
@@ -1,4 +1,5 @@
 // Aseprite UI Library
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -407,26 +408,40 @@ gfx::Size Graphics::doUIStringAlgorithm(const std::string& str, gfx::Color fg, g
   }
 
   gfx::Size calculatedSize(0, 0);
-  std::size_t beg, end, new_word_beg, old_end;
+  std::size_t beg, end, newBeg;
   std::string line;
   int lineSeparation = 2*guiscale();
 
   // Draw line-by-line
-  for (beg=end=0; end != std::string::npos; ) {
+  for (beg=end=0; end != std::string::npos && beg<str.size(); ) {
     pt.x = rc.x;
 
     // Without word-wrap
-    if ((align & WORDWRAP) == 0) {
+    if ((align & (WORDWRAP | CHARWRAP)) == 0) {
       end = str.find('\n', beg);
+      newBeg = end+1;
+    }
+    // With char-wrap
+    else if ((align & CHARWRAP) == CHARWRAP) {
+      for (end=beg+1; end<str.size(); ++end) {
+        // If we are out of the available width (rc.w) using the new "end"
+        if ((rc.w > 0) &&
+            (m_font->textLength(str.substr(beg, end-beg).c_str()) > rc.w)) {
+          if (end > beg+1)
+            --end;
+          break;
+        }
+      }
+      newBeg = end;
     }
     // With word-wrap
     else {
-      old_end = std::string::npos;
-      for (new_word_beg=beg;;) {
+      std::size_t old_end = std::string::npos;
+      for (std::size_t new_word_beg=beg;;) {
         end = str.find_first_of(" \n", new_word_beg);
 
         // If we have already a word to print (old_end != npos), and
-        // we are out of the available width (rc.w) using the new "end",
+        // we are out of the available width (rc.w) using the new "end"
         if ((old_end != std::string::npos) &&
             (rc.w > 0) &&
             (pt.x+m_font->textLength(str.substr(beg, end-beg).c_str()) > rc.w)) {
@@ -449,7 +464,10 @@ gfx::Size Graphics::doUIStringAlgorithm(const std::string& str, gfx::Color fg, g
 
         old_end = end;
       }
+      newBeg = end+1;
     }
+
+    ASSERT(beg < end);
 
     // Get the entire line to be painted
     line = str.substr(beg, end-beg);
@@ -479,7 +497,10 @@ gfx::Size Graphics::doUIStringAlgorithm(const std::string& str, gfx::Color fg, g
 
     pt.y += lineSize.h;
     calculatedSize.h += lineSize.h;
-    beg = end+1;
+    beg = newBeg;
+
+    if (pt.y+lineSize.h >= rc.y2())
+      break;
   }
 
   if (calculatedSize.h > 0)

data/extensions/aseprite-theme/theme.xml
@@ -194,6 +194,9 @@
         <part id="combobox_arrow_right_disabled" x="131" y="196" w="9" h="9" />
         <part id="newfolder" x="99" y="211" w="9" h="9" />
         <part id="newfolder_selected" x="115" y="211" w="9" h="9" />
+        <part id="list_view" x="96" y="224" w="9" h="9" />
+        <part id="small_icon_view" x="105" y="224" w="9" h="9" />
+        <part id="big_icon_view" x="114" y="224" w="9" h="9" />
         <part id="toolbutton_normal" x="96" y="0" w1="3" w2="10" w3="3" h1="3" h2="9" h3="4" />
         <part id="toolbutton_hot" x="112" y="0" w1="3" w2="10" w3="3" h1="3" h2="9" h3="4" />
         <part id="toolbutton_last" x="96" y="16" w1="3" w2="10" w3="3" h1="3" h2="9" h3="4" />
@@ -406,6 +409,9 @@
         <part id="icon_aspect_ratio" x="256" y="264" w="10" h="8" />
         <part id="linear_gradient" x="176" y="208" w="8" h="8" />
         <part id="radial_gradient" x="184" y="208" w="8" h="8" />
+        <part id="folder_icon_small" x="144" y="272" w="10" h="8" />
+        <part id="folder_icon_big" x="176" y="272" w="32" h="32" />
+        <part id="folder_icon_medium" x="160" y="272" w="16" h="16" />
     </parts>
     <styles>
         <style id="box" />
@@ -660,13 +666,13 @@
             <text color="disabled" align="left" x="2" />
         </style>
         <style id="recent_file_pin">
-          <background color="background" />
-          <background color="menuitem_hot_face" state="mouse" />
-          <background color="listitem_selected_face" state="selected" />
-          <icon part="unpinned" align="center middle" color="text" />
-          <icon part="unpinned" align="center middle" color="listitem_selected_text" state="selected" />
-          <icon part="pinned" align="center middle" color="text" state="focus" />
-          <icon part="pinned" align="center middle" color="listitem_selected_text" state="focus selected" />
+            <background color="background" />
+            <background color="menuitem_hot_face" state="mouse" />
+            <background color="listitem_selected_face" state="selected" />
+            <icon part="unpinned" align="center middle" color="text" />
+            <icon part="unpinned" align="center middle" color="listitem_selected_text" state="selected" />
+            <icon part="pinned" align="center middle" color="text" state="focus" />
+            <icon part="pinned" align="center middle" color="listitem_selected_text" state="focus selected" />
         </style>
         <style id="news_item" border="2">
             <background color="background" />

data/pref.xml
@@ -254,6 +254,10 @@
       <option id="advanced" type="bool" default="false" />
       <option id="pixel_ratio" type="std::string" />
     </section>
+    <section id="file_selector">
+      <option id="current_folder" type="std::string" default="&quot;&lt;empty&gt;&quot;" migrate="FileSelect.CurrentDirectory" />
+      <option id="zoom" type="double" default="1.0" />
+    </section>
     <section id="text_tool">
       <option id="font_face" type="std::string" />
       <option id="font_size" type="int" default="12" />

data/strings/en.ini
@@ -547,6 +547,9 @@ go_back_button_tooltip = Go back one folder
 go_forward_button_tooltip = Go forward one folder
 go_up_button_tooltip = Up to parent folder
 new_folder_button_tooltip = New folder
+list_view_button_tooltip = List View
+small_icon_view_button_tooltip = Small Icons View
+big_icon_view_button_tooltip = Big Icons View
 file_name = File name:
 file_type = File type:
 pinned_folders = Pinned Folders

data/widgets/file_selector.xml
@@ -1,15 +1,25 @@
 <!-- Aseprite -->
-<!-- Copyright (C) 2001-2018 by David Capello -->
+<!-- Copyright (C) 2019  Igara Studio S.A. -->
+<!-- Copyright (C) 2001-2018  David Capello -->
 <gui>
   <window id="file_selector" text="">
   <vbox id="main">
     <box horizontal="true">
-      <box horizontal="true" noborders="true">
-        <button text="" id="go_back_button" style="go_back_button" tooltip="@.go_back_button_tooltip" />
-        <button text="" id="go_forward_button" style="go_forward_button" tooltip="@.go_forward_button_tooltip" />
-      </box>
-      <button text="" id="go_up_button" style="go_up_button" tooltip="@.go_up_button_tooltip" />
-      <button text="" id="new_folder_button" style="new_folder_button" tooltip="@.new_folder_button_tooltip" />
+      <hbox noborders="true">
+        <button text="" id="go_back_button" style="go_back_button"
+                tooltip="@.go_back_button_tooltip" tooltip_dir="bottom" />
+        <button text="" id="go_forward_button" style="go_forward_button"
+                tooltip="@.go_forward_button_tooltip" tooltip_dir="bottom" />
+      </hbox>
+      <button text="" id="go_up_button" style="go_up_button"
+              tooltip="@.go_up_button_tooltip" tooltip_dir="bottom" />
+      <button text="" id="new_folder_button" style="new_folder_button"
+              tooltip="@.new_folder_button_tooltip" tooltip_dir="bottom" />
+      <buttonset id="view_type" columns="3">
+        <item icon="list_view" tooltip="@.list_view_button_tooltip" tooltip_dir="bottom" />
+        <item icon="small_icon_view" tooltip="@.small_icon_view_button_tooltip" tooltip_dir="bottom" />
+        <item icon="big_icon_view" tooltip="@.big_icon_view_button_tooltip" tooltip_dir="bottom" />
+      </buttonset>
       <combobox id="location" expansive="true" />
     </box>
     <vbox id="file_view_placeholder" expansive="true" />

src/app/commands/cmd_open_file.cpp
@@ -133,6 +133,7 @@ void OpenFileCommand::onExecute(Context* context)
 
   int flags =
     FILE_LOAD_DATA_FILE |
+    FILE_LOAD_CREATE_PALETTE |
     (m_repeatCheckbox ? FILE_LOAD_SEQUENCE_ASK_CHECKBOX: 0);
 
   switch (m_seqDecision) {

src/app/file/file.cpp
@@ -75,7 +75,11 @@ base::paths get_writable_extensions()
 Doc* load_document(Context* context, const std::string& filename)
 {
   /* TODO add a option to configure what to do with the sequence */
-  std::unique_ptr<FileOp> fop(FileOp::createLoadDocumentOperation(context, filename, FILE_LOAD_SEQUENCE_NONE));
+  std::unique_ptr<FileOp> fop(
+    FileOp::createLoadDocumentOperation(
+      context, filename,
+      FILE_LOAD_CREATE_PALETTE |
+      FILE_LOAD_SEQUENCE_NONE));
   if (!fop)
     return nullptr;
 
@@ -302,6 +306,9 @@ FileOp* FileOp::createLoadDocumentOperation(Context* context, const std::string&
   // Load just one frame
   if (flags & FILE_LOAD_ONE_FRAME)
     fop->m_oneframe = true;
+
+  if (flags & FILE_LOAD_CREATE_PALETTE)
+    fop->m_createPaletteFromRgba = true;
 
   // Does data file exist?
   if (flags & FILE_LOAD_DATA_FILE) {
@@ -887,7 +894,8 @@ void FileOp::postLoad()
   Sprite* sprite = m_document->sprite();
   if (sprite) {
     // Creates a suitable palette for RGB images
-    if (sprite->pixelFormat() == IMAGE_RGB &&
+    if (m_createPaletteFromRgba &&
+        sprite->pixelFormat() == IMAGE_RGB &&
         sprite->getPalettes().size() <= 1 &&
         sprite->palette(frame_t(0))->isBlack()) {
       base::SharedPtr<Palette> palette(
@@ -1176,6 +1184,7 @@ FileOp::FileOp(FileOpType type, Context* context)
   , m_done(false)
   , m_stop(false)
   , m_oneframe(false)
+  , m_createPaletteFromRgba(false)
   , m_ignoreEmpty(false)
   , m_preserveColorProfile(
       Preferences::instance().color.manage())

commit 6837168cebee93ea1f91ac9090388bd99422ea4b

Fix ColorShades() painting of default text
Regression introduced in 9a75d01efeaef8db9c61e01dbc9b5aae47c8f515

src/ui/graphics.cpp
@@ -433,7 +433,10 @@ gfx::Size Graphics::doUIStringAlgorithm(const std::string& str, gfx::Color fg, g
     // Without word-wrap
     if ((align & (WORDWRAP | CHARWRAP)) == 0) {
       end = str.find('\n', beg);
-      newBeg = end+1;
+      if (end != std::string::npos)
+        newBeg = end+1;
+      else
+        newBeg = std::string::npos;
     }
     // With char-wrap
     else if ((align & CHARWRAP) == CHARWRAP) {
@@ -485,7 +488,7 @@ gfx::Size Graphics::doUIStringAlgorithm(const std::string& str, gfx::Color fg, g
     if (end != std::string::npos)
       line = str.substr(beg, end-beg);
     else
-      line.clear();
+      line = str.substr(beg);
 
     gfx::Size lineSize(
       m_font->textLength(line.c_str()),