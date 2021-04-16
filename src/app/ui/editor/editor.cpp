evilguy50/aseprite

commit 21e893162d0a529019f75f91f3d7d25e2fdf55f0

Add possibility to hold tool modifiers when we start drawing on kMouseDownMessage (fix #2537)
E.g. In this way we can press the Shift modifier, then press the mouse
button to start drawing successive straight lines with the angle
snapped (so there is no need to release/press the Shift modifier again
and again).

src/app/ui/editor/editor.cpp
@@ -1536,7 +1536,7 @@ void Editor::updateToolByTipProximity(ui::PointerType pointerType)
   }
 }
 
-void Editor::updateToolLoopModifiersIndicators()
+void Editor::updateToolLoopModifiersIndicators(const bool firstFromMouseDown)
 {
   int modifiers = int(tools::ToolLoopModifiers::kNone);
   const bool autoSelectLayer = isAutoSelectLayer();
@@ -1569,7 +1569,13 @@ void Editor::updateToolLoopModifiersIndicators()
           modifiers |= int(tools::ToolLoopModifiers::kSquareAspect);
         if (int(action & KeyAction::DrawFromCenter))
           modifiers |= int(tools::ToolLoopModifiers::kFromCenter);
-        if (int(action & KeyAction::RotateShape))
+
+        // We prefer to activate the rotation only when the user press
+        // the Alt key again in the ToolLoop (and not before starting
+        // the loop). So Alt+Shift+selection tool will subtract the
+        // selection but will not start the rotation until we release
+        // and press the Alt key again.
+        if ((int(action & KeyAction::RotateShape)) && !firstFromMouseDown)
           modifiers |= int(tools::ToolLoopModifiers::kRotateShape);
       }
 
@@ -1800,7 +1806,16 @@ bool Editor::onProcessMessage(Message* msg)
           ->pressButton(pointer_from_msg(this, mouseMsg));
 
         EditorStatePtr holdState(m_state);
-        return m_state->onMouseDown(this, mouseMsg);
+        bool state = m_state->onMouseDown(this, mouseMsg);
+
+        // Re-update the tool modifiers if the state has changed
+        // (e.g. we are on DrawingState now). This is required for the
+        // Line tool to be able to Shift+press mouse buttons to start
+        // drawing lines with the angle snapped.
+        if (m_state != holdState)
+          updateToolLoopModifiersIndicators(true);
+
+        return state;
       }
       break;
 

src/app/ui/editor/editor.h
@@ -334,7 +334,10 @@ namespace app {
     void setStateInternal(const EditorStatePtr& newState);
     void updateQuicktool();
     void updateToolByTipProximity(ui::PointerType pointerType);
-    void updateToolLoopModifiersIndicators();
+
+    // firstFromMouseDown=true when we call this function from the
+    // first MouseDown message (instead of KeyDown).
+    void updateToolLoopModifiersIndicators(const bool firstFromMouseDown = false);
 
     void drawBackground(ui::Graphics* g);
     void drawSpriteUnclippedRect(ui::Graphics* g, const gfx::Rect& rc);

commit 1e2dfcc45829b93b80ec6e791379f743d45b3600

Don't activate square aspect when we start a Rectangular Marquee w/Shift modifier
Regression introduced in 21e893162d0a529019f75f91f3d7d25e2fdf55f0 to fix #2537

src/app/ui/editor/editor.cpp
@@ -1556,27 +1556,34 @@ void Editor::updateToolLoopModifiersIndicators(const bool firstFromMouseDown)
                      int(tools::ToolLoopModifiers::kSubtractSelection) |
                      int(tools::ToolLoopModifiers::kIntersectSelection)));
 
-      tools::Controller* controller =
-        (atm->selectedTool() ?
-         atm->selectedTool()->getController(0): nullptr);
+      tools::Tool* tool = atm->selectedTool();
+      tools::Controller* controller = (tool ? tool->getController(0): nullptr);
+      tools::Ink* ink = (tool ? tool->getInk(0): nullptr);
 
       // Shape tools modifiers (line, curves, rectangles, etc.)
       if (controller && controller->isTwoPoints()) {
         action = m_customizationDelegate->getPressedKeyAction(KeyContext::ShapeTool);
-        if (int(action & KeyAction::MoveOrigin))
-          modifiers |= int(tools::ToolLoopModifiers::kMoveOrigin);
-        if (int(action & KeyAction::SquareAspect))
-          modifiers |= int(tools::ToolLoopModifiers::kSquareAspect);
-        if (int(action & KeyAction::DrawFromCenter))
-          modifiers |= int(tools::ToolLoopModifiers::kFromCenter);
 
-        // We prefer to activate the rotation only when the user press
-        // the Alt key again in the ToolLoop (and not before starting
-        // the loop). So Alt+Shift+selection tool will subtract the
+        // For two-points-selection-like tools (Rectangular/Elliptical
+        // Marquee) we prefer to activate the
+        // square-aspect/rotation/etc. only when the user presses the
+        // modifier key again in the ToolLoop (and not before starting
+        // the loop). So Alt+selection will add a selection, but
+        // willn't start the square-aspect until we press Alt key
+        // again, or Alt+Shift+selection tool will subtract the
         // selection but will not start the rotation until we release
         // and press the Alt key again.
-        if ((int(action & KeyAction::RotateShape)) && !firstFromMouseDown)
-          modifiers |= int(tools::ToolLoopModifiers::kRotateShape);
+        if (!firstFromMouseDown ||
+            !ink || !ink->isSelection()) {
+          if (int(action & KeyAction::MoveOrigin))
+            modifiers |= int(tools::ToolLoopModifiers::kMoveOrigin);
+          if (int(action & KeyAction::SquareAspect))
+            modifiers |= int(tools::ToolLoopModifiers::kSquareAspect);
+          if (int(action & KeyAction::DrawFromCenter))
+            modifiers |= int(tools::ToolLoopModifiers::kFromCenter);
+          if (int(action & KeyAction::RotateShape))
+            modifiers |= int(tools::ToolLoopModifiers::kRotateShape);
+        }
       }
 
       // Freehand modifiers